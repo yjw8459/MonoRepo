355359700e17e6c89110d615097ac08cd5a4ab72
YooJongwon <jwyoo@osstem.com>
2022-10-17 11:25:24 +0900

    electron error 1

--- packages/vue-test/vue.config.js
+++ packages/vue-test/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
    * Symbolic Links in node_modules
    * @see {@link https://cli.vuejs.org/guide/troubleshooting.html#running-installation-with-sudo-or-as-root}
    */
-    chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.symlinks(false);
   },
   transpileDependencies: true,
